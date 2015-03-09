6d069d9f27d74bf8e9a15e05e5de31e5dd83f476
Mario Rogic <hello@mario.net.au>
2015-03-09 19:01:57 +1100

    Don't need uid switching, we take care of it in the Dockerfile

--- nginx-phpfpm/nginx/init.sh
+++ nginx-phpfpm/nginx/init.sh
@@ -18,25 +18,3 @@ fi
 
 sed -i "s#root .*;#root $NGINX_DOCROOT;#" /etc/nginx/sites-enabled/default
 mkdir -p $NGINX_DOCROOT
-
-if [ -z ${SYNC_UID+x} ]; then
-    if [ $SYNC_UID -eq 1 ];
-    then
-        uid=`stat -c '%u' $DOCROOT`
-        gid=`stat -c '%g' $DOCROOT`
-
-        echo "Updating www-data ids to ($uid:$gid)"
-
-        if [ ! $uid -eq 0 ]
-        then
-            sed -i "s#^www-data:x:.*:.*:#www-data:x:$uid:$gid:#" /etc/passwd
-        fi
-
-        if [ ! $gid -eq 0 ]
-        then
-            sed -i "s#^www-data:x:.*:#www-data:x:$gid:#" /etc/group
-        fi
-
-        chown www-data /var/log/nginx
-    fi
-fi
