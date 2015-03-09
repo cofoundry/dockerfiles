63fb11a5b133bc621918cfc57bdee4a192b38aad
Mario Rogic <hello@mario.net.au>
2015-03-09 15:07:15 +1100

    Nginx conf / site included but overridable

--- nginx-phpfpm/nginx/init.sh
+++ nginx-phpfpm/nginx/init.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
+if [ -n "${NGINX_CONF}"]
+then
+    cp $NGINX_CONF /etc/nginx/nginx.conf
+fi
+
+if [ -n "${SITE_CONF}"]
+then
+    cp $SITE_CONF /etc/nginx/sites-enabled/default
+fi
+
 NGINX_DOCROOT="/app"
 if [ -n "${DOCROOT}" ]
 then
